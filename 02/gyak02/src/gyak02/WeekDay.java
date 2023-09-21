a30da8dee6504e5bc084ac0104ed0ee3d5256693
Sándor Balázs <sabuaai@INF.ELTE.HU>
2023-09-21 13:44:14 +0200

    További órai munka

--- 02/gyak02/src/gyak02/WeekDay.java
+++ 02/gyak02/src/gyak02/WeekDay.java
@@ -1,21 +1,37 @@
 package gyak02;
 
 enum WeekDay {
-	MON,
-	TUE,
-	WED,
-	THU,
-	FRI,
-	SAT,
-	SUN;
+	MON("HET"),
+	TUE("KED"),
+	WED("SZE"),
+	THU("CSÜ"),
+	FRI("PEN"),
+	SAT("SZO"),
+	SUN("VAS");
 	
-	public WeekDay nextDay(){
+	//final és a const nem ugyan az.
+	//String inmutable
+	private String huName;
+	
+	WeekDay(String huName) {
+		this.huName = huName;
+	}
+	
+	public String getHuName() {
+		return this.huName;
+	}
+	
+	public void setHuName(String huName) {
+		this.huName = huName;
+	}
+	
+	public WeekDay nextDay() {
 		 //return values()[3];
 		//return values()[ordinal()+1];
 		return values()[(ordinal() + 1) % values().length];
 	}
 	
-	public WeekDay nextDay(int shift){
+	public WeekDay nextDay(int shift) {
 		return values()[(ordinal() + shift) % values().length];
 	}
 }
